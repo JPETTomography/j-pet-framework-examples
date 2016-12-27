6757d8b12fd066936de4a6f11e28d4986c6e5457
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-12-27 18:39:11 +0100

    Add module to apply prepared time calibration
    Also, time calib tools are added.

--- LargeBarrelAnalysis/main.cpp
+++ LargeBarrelAnalysis/main.cpp
@@ -17,6 +17,7 @@
 #include <JPetManager/JPetManager.h>
 #include <JPetTaskLoader/JPetTaskLoader.h>
 #include "TaskA.h"
+#include "TimeCalibLoader.h"
 #include "TaskB1.h"
 #include "TaskC.h"
 #include "TaskD.h"
@@ -24,11 +25,12 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-	DB::SERVICES::DBHandler::createDBConnection("../DBConfig/configDB.cfg");
+int main(int argc, char* argv[])
+{
+  DB::SERVICES::DBHandler::createDBConnection("../DBConfig/configDB.cfg");
   JPetManager& manager = JPetManager::getManager();
   manager.parseCmdLine(argc, argv);
-  
+
   // Here create all analysis modules to be used:
 
   // manager.registerTask([](){
@@ -36,25 +38,31 @@ int main(int argc, char* argv[]) {
   // 				  new TaskA("Module A: Unp to TSlot Raw",
   // 					    "Process unpacked HLD file into a tree of JPetTSlot objects"));
   //   });
-  
+
+  //manager.registerTask([]() {
+    //return new JPetTaskLoader("tslot.raw", "tslot.raw",
+                              //new TimeCalibLoader("Apply time corrections from prepared calibrations",
+                                         //"Apply time corrections from prepared calibrations"));
+  //});
+
   // manager.registerTask([](){
   //     return new JPetTaskLoader("tslot.raw", "raw.sig",
   // 				new TaskB1("Module B1: Make TOT histos and assemble signals",
   // 					   "Assemble signals and create TOT historgrams"));
   //   });
 
-   manager.registerTask([](){ 
-       return new JPetTaskLoader("raw.sig", "phys.hit", 
-				 new TaskC("Module C: Pair signals", 
-					   "Create hits from pairs of signals")); 
-     }); 
+  manager.registerTask([]() {
+    return new JPetTaskLoader("raw.sig", "phys.hit",
+                              new TaskC("Module C: Pair signals",
+                                        "Create hits from pairs of signals"));
+  });
 
   // manager.registerTask([](){
   //     return new JPetTaskLoader("phys.hit", "phys.hit.means",
   // 				new TaskD("Module D: Make histograms for hits",
   // 					  "Only make timeDiff histos and produce mean timeDiff value for each threshold and slot to be used by the next module"));
   //   });
-  
+
 
   // manager.registerTask([](){
   //     return new JPetTaskLoader("phys.hit.means", "phys.hit.coincplots",
