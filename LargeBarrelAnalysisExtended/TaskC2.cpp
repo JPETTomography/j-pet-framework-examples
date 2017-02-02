e45aac98cf4891778e6ccbfb56fa5012975d1734
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-02-02 10:26:32 +0100

    Add conditions checks in case of not efficient number of points in Signals in TaskC2

--- LargeBarrelAnalysisExtended/TaskC2.cpp
+++ LargeBarrelAnalysisExtended/TaskC2.cpp
@@ -56,43 +56,53 @@ JPetRecoSignal TaskC2::createRecoSignal(JPetRawSignal& rawSignal)
   // JPetRawSignal::ByThrNum option instead which will sort the SigCh-s by the number
   // of threshold on its edge (1,..,4 in this case) rather by threshold value
 
-  // on each edge, take the first (smallest) threshold and its corresponding time
-  double t1 = leadingPoints.front().getValue();
-  double t2 = trailingPoints.front().getValue();
-  // note that there is no guarantee that these two smallest thresholds will actually
-  // have the same threshold value: for some reason, time on the smallest threshold
-  // might not have been recorded on one of the edges
-  // this should be checked, but it is beyond the scope of this simple example
+  double t1 = 0;
+  double t2 = 0;
+  if ((trailingPoints.size() == 0u) || (leadingPoints.size() == 0u)) {
+    WARNING("Not enough points to calculate TOT from RawSignal");
+  } else {
 
-  // finally, calculate the ToT
-  double ToT = t2 - t1;
+    // on each edge, take the first (smallest) threshold and its corresponding time
+    t1 = leadingPoints.front().getValue();
+    t2 = trailingPoints.front().getValue();
+    // note that there is no guarantee that these two smallest thresholds will actually
+    // have the same threshold value: for some reason, time on the smallest threshold
+    // might not have been recorded on one of the edges
+    // this should be checked, but it is beyond the scope of this simple example
 
-  // now that we have the ToT, we can calculate charge from it
-  // here, a dummy calculation
-  double charge = ToT * 1.0 + 0.0;
+    // finally, calculate the ToT
+    double ToT = t2 - t1;
 
-  // and write the tot value to a histogram
-  getStatistics().getHisto1D("tot").Fill(ToT);
+    // now that we have the ToT, we can calculate charge from it
+    // here, a dummy calculation
+    double charge = ToT * 1.0 + 0.0;
 
-  // store the newly calculated charge value in the RecoSignal
-  recoSignal.setCharge( charge );
+    // and write the tot value to a histogram
+    getStatistics().getHisto1D("tot").Fill(ToT);
 
+    // store the newly calculated charge value in the RecoSignal
+    recoSignal.setCharge( charge );
+  }
   // similarly to the above example, we can calculate other properties
   // and store them
   recoSignal.setDelay( 0.0 );
   recoSignal.setOffset( 0.0 );
   recoSignal.setAmplitude( 0.0 );
 
-  // one more silly example - calculate time at arbitrary threshold by "interpolation"
-  // between lowest and second-lowest threshold measured by DAQ on leading edge
-  double thr1, thr2;
-  t1 = rawSignal.getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrValue).at(0).getValue();
-  thr1 = rawSignal.getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrValue).at(0).getThreshold();
-  t2 = rawSignal.getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrValue).at(1).getValue();
-  thr2 = rawSignal.getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrValue).at(1).getThreshold();
+  if (rawSignal.getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrValue).size() < 2 ) {
+    WARNING("Not enough leading points to calculate time at threshold from RawSignal");
+  } else {
+    // one more silly example - calculate time at arbitrary threshold by "interpolation"
+    // between lowest and second-lowest threshold measured by DAQ on leading edge
+    double thr1, thr2;
+    t1 = rawSignal.getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrValue).at(0).getValue();
+    thr1 = rawSignal.getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrValue).at(0).getThreshold();
+    t2 = rawSignal.getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrValue).at(1).getValue();
+    thr2 = rawSignal.getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrValue).at(1).getThreshold();
 
-  recoSignal.setRecoTimeAtThreshold((thr1 + thr2) / 2., (t1 + t2) / 2. );
-  //recoSignal.setRecoTimeAtThreshold(10., 20. );
+    recoSignal.setRecoTimeAtThreshold((thr1 + thr2) / 2., (t1 + t2) / 2. );
+    //recoSignal.setRecoTimeAtThreshold(10., 20. );
+  }
 
   // store the original JPetRawSignal in the RecoSignal as a processing history
   recoSignal.setRawSignal(rawSignal);
