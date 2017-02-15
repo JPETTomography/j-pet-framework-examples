929f8185c4f56baba65a8f2c3a69cfff6c39affb
Krzysztof Kacprzak <k.kacprzak@yahoo.com>
2017-02-15 15:44:31 +0100

    HitFinder Cleanup and comment adding

--- LargeBarrelAnalysisExtended/HitFinderTools.cpp
+++ LargeBarrelAnalysisExtended/HitFinderTools.cpp
@@ -17,50 +17,65 @@
 
 using namespace std;
 
-vector<JPetHit> HitFinderTools::createHits(const SignalsContainer& allSignalsInTimeWindow, const double timeDifferenceWindow)
+vector<JPetHit> HitFinderTools::createHits(const SignalsContainer& allSignalsInTimeWindow,
+						const double timeDifferenceWindow)
 {
+	vector<JPetHit> hits;
 
-  vector<JPetHit> hits;
+	for (auto scintillator : allSignalsInTimeWindow) {
 
-  for (auto scintillator : allSignalsInTimeWindow) {
+		auto sideA = scintillator.second.first;
+		auto sideB = scintillator.second.second;
 
-    auto sideA = scintillator.second.first;
-    auto sideB = scintillator.second.second;
+		if (sideA.size() > 0 && sideB.size() > 0) {
 
-    if (sideA.size() > 0 and sideB.size() > 0) {
+			std::sort(sideA.begin(),
+				sideA.end(),
+				[] (const JPetPhysSignal & h1,
+					const JPetPhysSignal & h2) {
+					return h1.getTime() < h2.getTime();
+				});
 
-      std::sort(sideA.begin(), sideA.end(), [] (const JPetPhysSignal & h1, const JPetPhysSignal & h2) {
-        return h1.getTime() < h2.getTime();
-      });
-      std::sort(sideB.begin(), sideB.end(), [] (const JPetPhysSignal & h1, const JPetPhysSignal & h2) {
-        return h1.getTime() < h2.getTime();
-      });
+			std::sort(sideB.begin(),
+				sideB.end(),
+				[] (const JPetPhysSignal & h1,
+					const JPetPhysSignal & h2) {
+				        return h1.getTime() < h2.getTime();
+				});
 
-      for (auto signalA : sideA) {
-        for (auto signalB : sideB) {
+			for (auto signalA : sideA) {
+				for (auto signalB : sideB) {
 
-          if ( (signalB.getTime() - signalA.getTime()) > timeDifferenceWindow) {
-            break;
-          }
+					if ((signalB.getTime() - signalA.getTime())
+								> timeDifferenceWindow)
+									break;
 
-          if (abs(signalA.getTime() - signalB.getTime()) < timeDifferenceWindow /*ps*/) {
+					if (fabs(signalA.getTime() - signalB.getTime())
+								< timeDifferenceWindow) {
 
-            JPetHit hit;
+						//Creating hit for successfully matched pair of Phys singlas
+						//Setting meaningless parameters of Energy, Position, quality
+						JPetHit hit;
+						hit.setSignalA(signalA);
+						hit.setSignalB(signalB);
+						hit.setTime((signalA.getTime()
+								+ signalB.getTime())/2.0);
+						hit.setQualityOfTime(-1.0);
+						hit.setTimeDiff(fabs(signalA.getTime()
+								- signalB.getTime()));
+						hit.setQualityOfTimeDiff(-1.0);
+						hit.setEnergy(-1.0);
+						hit.setQualityOfEnergy(-1.0);
+						hit.setPosAlongStrip(-1.0);
 
-            hit.setSignalA(signalA);
-            hit.setSignalB(signalB);
-            hit.setTime((signalA.getTime() + signalB.getTime()) / 2.0);
-	    hit.setTimeDiff(fabs(signalA.getTime() - signalB.getTime()));
+            					hit.setScintillator(signalA.getPM().getScin());
+						hit.setBarrelSlot(signalA.getPM().getBarrelSlot());
 
-            hit.setScintillator(signalA.getPM().getScin());
-            hit.setBarrelSlot(signalA.getPM().getBarrelSlot());
-
-            hits.push_back(hit);
-          }
-        }
-      }
-
-    }
-  }
-  return hits;
+						hits.push_back(hit);
+					}
+				}
+			}
+		}
+	}
+	return hits;
 }
