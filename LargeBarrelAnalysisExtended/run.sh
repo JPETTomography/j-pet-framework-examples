4db13c92c361533dd489ee12ddc3c0ec8959457e
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-03-02 01:02:44 +0100

    Change download_data.sh to sphinx server.

--- LargeBarrelAnalysisExtended/run.sh
+++ LargeBarrelAnalysisExtended/run.sh
@@ -1,1 +1,1 @@
-./LargeBarrelAnalysisExtended.x -t hld -f <data file name>.hld -p conf_trb3.xml -u userParams.json -i 43 
+./LargeBarrelAnalysisExtended.x -t hld -f <data file name>.hld -p conf_trb3.xml -u userParams.json -i 43 -l large_barrel.json
