3092f4bfba4d9e7372c7218d8da391bb6556b554
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-04-04 22:04:51 +0200

    Fix bug in HitFinderTools
    The theta angle is given in deg, and it should be translated to rad
    when calculating sin and cos for x,y coordinates of hits.
    It was fixed. Also explicit headers were added and some formatting was
    corrected.

--- LargeBarrelAnalysisExtended/HitFinderTools.cpp
+++ LargeBarrelAnalysisExtended/HitFinderTools.cpp
@@ -14,87 +14,88 @@
  */
 
 #include "HitFinderTools.h"
+#include <cmath> /// std::sin(), std::cos()
+#include <TMath.h> /// DegToRad()
 
 using namespace std;
 
 vector<JPetHit> HitFinderTools::createHits(JPetStatistics& stats,
-  const SignalsContainer& allSignalsInTimeWindow,
-  const double timeDifferenceWindow,
-  const std::map<int, std::vector<double>> velMap)
+    const SignalsContainer& allSignalsInTimeWindow,
+    const double timeDifferenceWindow,
+    const std::map<int, std::vector<double>> velMap)
 {
-	vector<JPetHit> hits;
+  vector<JPetHit> hits;
 
-	for (auto scintillator : allSignalsInTimeWindow) {
+  for (auto scintillator : allSignalsInTimeWindow) {
 
-		auto sideA = scintillator.second.first;
-		auto sideB = scintillator.second.second;
+    auto sideA = scintillator.second.first;
+    auto sideB = scintillator.second.second;
 
-		if (sideA.size() > 0 && sideB.size() > 0) {
+    if (sideA.size() > 0 && sideB.size() > 0) {
 
-			std::sort(sideA.begin(),
-				sideA.end(),
-				[] (const JPetPhysSignal & h1,
-					const JPetPhysSignal & h2) {
-					return h1.getTime() < h2.getTime();
-				});
+      std::sort(sideA.begin(),
+                sideA.end(),
+                [] (const JPetPhysSignal & h1,
+      const JPetPhysSignal & h2) {
+        return h1.getTime() < h2.getTime();
+      });
 
-			std::sort(sideB.begin(),
-				sideB.end(),
-				[] (const JPetPhysSignal & h1,
-					const JPetPhysSignal & h2) {
-				        return h1.getTime() < h2.getTime();
-				});
+      std::sort(sideB.begin(),
+                sideB.end(),
+                [] (const JPetPhysSignal & h1,
+      const JPetPhysSignal & h2) {
+        return h1.getTime() < h2.getTime();
+      });
 
-			for (auto signalA : sideA) {
-				for (auto signalB : sideB) {
+      for (auto signalA : sideA) {
+        for (auto signalB : sideB) {
 
-					if ((signalB.getTime() - signalA.getTime())
-								> timeDifferenceWindow)
-									break;
+          if ((signalB.getTime() - signalA.getTime())
+              > timeDifferenceWindow)
+            break;
 
-					if (fabs(signalA.getTime() - signalB.getTime())
-								< timeDifferenceWindow) {
+          if (fabs(signalA.getTime() - signalB.getTime())
+              < timeDifferenceWindow) {
 
-						//Creating hit for successfully matched pair of Phys singlas
-						//Setting meaningless parameters of Energy, Position, quality
-						JPetHit hit;
-						hit.setSignalA(signalA);
-						hit.setSignalB(signalB);
-						hit.setTime((signalA.getTime()+signalB.getTime())/2.0);
-						hit.setQualityOfTime(-1.0);
-						hit.setTimeDiff(signalA.getTime()-signalB.getTime());
-						hit.setQualityOfTimeDiff(-1.0);
-						hit.setEnergy(-1.0);
-						hit.setQualityOfEnergy(-1.0);
+            //Creating hit for successfully matched pair of Phys singlas
+            //Setting meaningless parameters of Energy, Position, quality
+            JPetHit hit;
+            hit.setSignalA(signalA);
+            hit.setSignalB(signalB);
+            hit.setTime((signalA.getTime() + signalB.getTime()) / 2.0);
+            hit.setQualityOfTime(-1.0);
+            hit.setTimeDiff(signalA.getTime() - signalB.getTime());
+            hit.setQualityOfTimeDiff(-1.0);
+            hit.setEnergy(-1.0);
+            hit.setQualityOfEnergy(-1.0);
             hit.setScintillator(signalA.getPM().getScin());
-						hit.setBarrelSlot(signalA.getPM().getBarrelSlot());
-						hit.setPosX(hit.getBarrelSlot().getLayer().getRadius()
-												*cos(hit.getBarrelSlot().getTheta()));
-						hit.setPosY(hit.getBarrelSlot().getLayer().getRadius()
-												*sin(hit.getBarrelSlot().getTheta()));
+            hit.setBarrelSlot(signalA.getPM().getBarrelSlot());
+            auto radius = hit.getBarrelSlot().getLayer().getRadius();
+            auto theta = TMath::DegToRad() * hit.getBarrelSlot().getTheta();
+            hit.setPosX(radius * std::cos(theta));
+            hit.setPosY(radius * std::sin(theta));
+            auto search = velMap.find(hit.getBarrelSlot().getID());
+            if (search != velMap.end()) {
+              double vel = search->second.at(0);
+              double position = vel * hit.getTimeDiff() / 2000;
+              hit.setPosZ(position);
+            } else {
+              hit.setPosZ(-1000000.0);
+            }
 
-						auto search = velMap.find(hit.getBarrelSlot().getID());
-						if(search != velMap.end()){
-							double vel = search->second.at(0);
-							double position = vel*hit.getTimeDiff()/2000;
-							hit.setPosZ(position);
-						}else{
-							hit.setPosZ(-1000000.0);
-						}
-
-						hits.push_back(hit);
+            hits.push_back(hit);
 
             stats.getHisto2D("time_diff_per_scin")
-              .Fill(hit.getTimeDiff(),
-                (float) (hit.getScintillator().getID()));
+            .Fill(hit.getTimeDiff(),
+                  (float) (hit.getScintillator().getID()));
 
-						stats.getHisto2D("hit_pos_per_scin")
-							.Fill(hit.getPosZ(),
-                (float) (hit.getScintillator().getID()));
-					}
-				}
-			}
-		}
-	}
-	return hits;
+            stats.getHisto2D("hit_pos_per_scin")
+            .Fill(hit.getPosZ(),
+                  (float) (hit.getScintillator().getID()));
+          }
+        }
+      }
+    }
+  }
+  return hits;
 }
