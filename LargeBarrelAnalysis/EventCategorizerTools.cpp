c9f19e1565c4ffdf654a6aef347dedac869ac84f
wictus <wictusn@gmail.com>
2017-12-29 23:23:52 +0100

    Add new tools for EventCategorizer
    Add tools for calculation of TOF according to convention and
    annihilation position calculated from TOF of two hits.
    Add tests to make sure everything works properly.

---
+++ LargeBarrelAnalysis/EventCategorizerTools.cpp
@@ -0,0 +1,63 @@
+#include "EventCategorizerTools.h"
+
+Point3D EventCategorizerTools::calculateAnnihilationPoint(const JPetHit& firstHit, const JPetHit& latterHit)
+{
+  Point3D annihilationPoint;
+  annihilationPoint.x = 999.0;
+  annihilationPoint.y = 999.0;
+  annihilationPoint.z = 999.0;
+  
+  Point3D firstHitPoint;
+  firstHitPoint.x = firstHit.getPosX();
+  firstHitPoint.y = firstHit.getPosY();
+  firstHitPoint.z = firstHit.getPosZ();
+  
+  Point3D latterHitPoint;
+  latterHitPoint.x = latterHit.getPosX();
+  latterHitPoint.y = latterHit.getPosY();
+  latterHitPoint.z = latterHit.getPosZ();
+  
+  Point3D middleOfLOR;
+  middleOfLOR.x = ( firstHitPoint.x + latterHitPoint.x ) / 2.0;
+  middleOfLOR.y = ( firstHitPoint.y + latterHitPoint.y ) / 2.0;
+  middleOfLOR.z = ( firstHitPoint.z + latterHitPoint.z ) / 2.0;
+  
+  double LORlength = sqrt( pow(( firstHitPoint.x - latterHitPoint.x),2) +
+		    pow((firstHitPoint.y - latterHitPoint.y),2) +
+		    pow((firstHitPoint.z - latterHitPoint.z),2) );
+  
+  std::vector<double> versorOnLOR;  //towards first hit
+  versorOnLOR.push_back( fabs( (latterHitPoint.x - firstHitPoint.x)/LORlength ) );
+  versorOnLOR.push_back( fabs( (latterHitPoint.y - firstHitPoint.y)/LORlength ) );
+  versorOnLOR.push_back( fabs( (latterHitPoint.z - firstHitPoint.z)/LORlength ) );
+  
+  double velocity = 30.0; //cm/ns
+  velocity/=1000; //cm/ps
+  
+  annihilationPoint.x = middleOfLOR.x - versorOnLOR[0]* calculateTOF(firstHit, latterHit)*velocity;
+  annihilationPoint.y = middleOfLOR.y - versorOnLOR[1]* calculateTOF(firstHit, latterHit)*velocity;
+  annihilationPoint.z = middleOfLOR.z - versorOnLOR[2]* calculateTOF(firstHit, latterHit)*velocity;
+  
+  return annihilationPoint;
+}
+
+double EventCategorizerTools::calculateTOF(const JPetHit& firstHit, const JPetHit& latterHit)
+{
+  double TOF = 9999;
+  if( firstHit.getTime() > latterHit.getTime() )
+  {
+    ERROR("First hit time should be earlier than later hit");
+    return TOF;
+  }
+  TOF = firstHit.getTime()-latterHit.getTime();
+  
+  double firstHitSlotAngle = firstHit.getBarrelSlot().getTheta();
+  double latterHitSlotAngle = latterHit.getBarrelSlot().getTheta();
+  
+  if( firstHitSlotAngle < latterHitSlotAngle )
+    return TOF;
+  else 
+    return -1.0*TOF;
+  
+  return TOF;
+}
---
+++ LargeBarrelAnalysis/EventCategorizerTools.h
@@ -0,0 +1,36 @@
+/**
+ *  @copyright Copyright 2017 The J-PET Framework Authors. All rights reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may find a copy of the License in the LICENCE file.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  @file EventCategorizerTools.h
+ */
+
+#ifndef _EVENTCATEGORIZERTOOLS_H_
+#define _EVENTCATEGORIZERTOOLS_H_
+
+#include <DataObjects/JPetHit/JPetHit.h>
+
+struct Point3D
+{
+  double x=0;
+  double y=0;
+  double z=0;
+};
+
+
+class EventCategorizerTools
+{
+public:
+    static double calculateTOF(const JPetHit& firstHit, const JPetHit& latterHit);
+    static Point3D calculateAnnihilationPoint(const JPetHit& firstHit, const JPetHit& latterHit);
+};
+
+#endif /*  !EVENTCATEGORIZERTOOLS_H */
