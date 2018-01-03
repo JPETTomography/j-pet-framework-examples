658a165c56f594443f651fed5a1d8b6cbff63970
kdulski <kamil.dulski@gmail.com>
2018-01-03 13:49:09 +0100

    Adding folders for streams - deleting old folders

--- LargeBarrelAnalysis/main.cpp
+++ LargeBarrelAnalysis/main.cpp
@@ -29,20 +29,20 @@ int main(int argc, const char* argv[])
 
   JPetManager& manager = JPetManager::getManager();
 
-  manager.registerTask<TimeWindowCreator>("TimeWindowCreator");
+ /* manager.registerTask<TimeWindowCreator>("TimeWindowCreator");
   manager.registerTask<TimeCalibLoader>("TimeCalibLoader");
   manager.registerTask<SignalFinder>("SignalFinder");
   manager.registerTask<SignalTransformer>("SignalTransformer"); 
   manager.registerTask<HitFinder>("HitFinder"); 
-  manager.registerTask<EventFinder>("EventFinder"); 
+  manager.registerTask<EventFinder>("EventFinder"); */
   manager.registerTask<EventCategorizer>("EventCategorizer"); 
   
-  manager.useTask("TimeWindowCreator", "hld", "tslot.raw");
+ /* manager.useTask("TimeWindowCreator", "hld", "tslot.raw");
   manager.useTask("TimeCalibLoader", "tslot.raw", "tslot.calib");
   manager.useTask("SignalFinder", "tslot.calib", "raw.sig");
   manager.useTask("SignalTransformer", "raw.sig", "phys.sig");
   manager.useTask("HitFinder", "phys.sig", "hits");
-  manager.useTask("EventFinder", "hits", "unk.evt");
+  manager.useTask("EventFinder", "hits", "unk.evt");*/
   manager.useTask("EventCategorizer", "unk.evt", "cat.evt");
   
   manager.run(argc, argv);
