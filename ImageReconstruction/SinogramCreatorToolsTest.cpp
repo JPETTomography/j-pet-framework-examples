e51c6c0f87a759eab629c27eb64f97d044ca3e96
Kamil Rakoczy <grey277@wp.pl>
2018-03-24 20:12:26 +0100

    Add intersection point not in middle test, remove MC test

--- ImageReconstruction/SinogramCreatorToolsTest.cpp
+++ ImageReconstruction/SinogramCreatorToolsTest.cpp
@@ -68,9 +68,6 @@ BOOST_AUTO_TEST_CASE( bresenham_test )
 BOOST_AUTO_TEST_CASE(xiaolin_test)
 {
   auto result = SinogramCreatorTools::calculateLineWithAntialiasing(0., 1., 6., 4.);
-  //for (const auto& r : result) {
-  //  std::cout << std::get<0>(r) << " " << std::get<1>(r) << " " << std::get<2>(r) << std::endl;
-  //}
   BOOST_REQUIRE_EQUAL(result.size(), 14);
   BOOST_REQUIRE_EQUAL(result[0], std::make_tuple(0, 1, 128));
   BOOST_REQUIRE_EQUAL(result[1], std::make_tuple(0, 2, 0));
@@ -138,6 +135,25 @@ BOOST_AUTO_TEST_CASE( test_line_intersection_and_distance )
   BOOST_REQUIRE_CLOSE(result.first, 0.5f, EPSILON);
   BOOST_REQUIRE_CLOSE(result.second, -0.5f, EPSILON);
   BOOST_REQUIRE_CLOSE(SinogramCreatorTools::length2D(result.first, result.second), 0.707106781f, EPSILON);
+
+  const float interesectionScale = 1.f / 10.f;
+  for (float alpha = 0.1f; alpha < 180.f; alpha += 0.1f) {
+    float x = 10 * cos(alpha * (M_PI / 180.f));
+    float y = 10 * sin(alpha * (M_PI / 180.f));
+    float scaledX = x * interesectionScale;
+    float scaledY = y * interesectionScale;
+    float dx = scaledX + scaledX;
+    float dy = scaledY + scaledY;
+    float a1 = scaledX - dy / 2;
+    float b1 = scaledY + dx / 2;
+    float a2 = scaledX + dy / 2;
+    float b2 = scaledY - dx / 2;
+    auto result = SinogramCreatorTools::lineIntersection(std::make_pair(a1, b1), std::make_pair(a2, b2), std::make_pair(x, y), std::make_pair(-x, -y));
+    float distance = std::sqrt((scaledX * scaledX) + (scaledY * scaledY));
+    BOOST_REQUIRE_CLOSE(result.first, scaledX, EPSILON);
+    BOOST_REQUIRE_CLOSE(result.second, scaledY, EPSILON);
+    BOOST_REQUIRE_CLOSE(SinogramCreatorTools::length2D(result.first, result.second), distance, EPSILON);
+  }
 }
 
 BOOST_AUTO_TEST_CASE(roundToNearesMultiplicity_test)
@@ -157,73 +173,5 @@ BOOST_AUTO_TEST_CASE(roundToNearesMultiplicity_test)
   BOOST_REQUIRE_EQUAL(SinogramCreatorTools::roundToNearesMultiplicity(-30.f, 0.01f, 30.f), 0u);
   BOOST_REQUIRE_EQUAL(SinogramCreatorTools::roundToNearesMultiplicity(-29.9f, 0.01f, 30.f), 10u);
 }
-
-/*BOOST_AUTO_TEST_CASE( test_MC_data )
-{
-  const std::string inFile = "unitTestData/SinogramCreatorToolsTest/mc_data.txt";
-  const std::string fOutFileName = "result.ppm";
-  std::vector<std::pair<SinogramCreatorTools::Point, SinogramCreatorTools::Point>> hits;
-  std::ifstream in(inFile);
-  BOOST_REQUIRE(in);
-  float x1, y1, x2, y2;
-  while (in.peek() != EOF) {
-    in >> x1 >> y1 >> x2 >> y2;
-    hits.push_back(std::make_pair(std::make_pair(x1, y1), std::make_pair(x2, y2)));
-  }
-  float fReconstructionStartAngle = 0.;
-  float fReconstructionEndAngle = 180.;
-  float fReconstructionAngleStep = 1.;
-  float fReconstructionLayerRadius =  10.;
-  float fReconstructionDistanceAccuracy = 1;
-  using SinogramResultType = std::vector<std::vector<unsigned int>>;
-  SinogramResultType* fSinogram = nullptr;
-  unsigned int maxThetaNumber = std::ceil(180. / fReconstructionAngleStep);
-  unsigned int maxDistanceNumber = std::ceil(fReconstructionLayerRadius * 2 * (1.f / fReconstructionDistanceAccuracy)) + 1;
-  if (fSinogram == nullptr) {
-    fSinogram = new SinogramResultType(maxDistanceNumber, (std::vector<unsigned int>(maxThetaNumber)));
-  }
-  unsigned int currentValueInSinogram = 0;
-  unsigned int fMaxValueInSinogram = 0;
-  std::cout << "Number of hits: " << hits.size() << std::endl;
-  std::cout << "Hit[0]: x:" << hits[0].first.first << " y:" << hits[0].first.second << " x:" << hits[0].second.first << " y:" << hits[0].second.second << std::endl;
-  for (int i = 0; i < hits.size(); i++) {
-    for (float theta = fReconstructionStartAngle; theta < fReconstructionEndAngle; theta += fReconstructionAngleStep) {
-      float x = fReconstructionLayerRadius * std::cos(theta * (M_PI / 180.f)); // calculate x,y positon of line with theta angle from line (0,0) = theta
-      float y = fReconstructionLayerRadius * std::sin(theta * (M_PI / 180.f));
-      std::pair<float, float> intersectionPoint = SinogramCreatorTools::lineIntersection(std::make_pair(-x, -y), std::make_pair(x, y),
-          hits[i].first, hits[i].second); //find intersection point
-      if (intersectionPoint.first != std::numeric_limits<float>::max() && intersectionPoint.second != std::numeric_limits<float>::max()) {
-        // check is there is intersection point
-        float distance = SinogramCreatorTools::length2D(intersectionPoint.first, intersectionPoint.second);
-        if (std::abs(distance) < 0.000001) {
-          std::cout << "Distance: " << distance << " intersectionPoint: " << intersectionPoint.first << "," << intersectionPoint.second << " x,y: " << x << "," << y << " p1: " << hits[i].first.first << "," << hits[i].first.second << " p2: " << hits[i].second.first << "," << hits[i].second.second << std::endl;
-        }
-        if (distance >= fReconstructionLayerRadius) // if distance is greather then our max reconstuction layer radius, it cant be placed in sinogram
-          continue;
-        if (intersectionPoint.first < 0.f)
-          distance = -distance;
-        int distanceRound = std::floor((fReconstructionLayerRadius / fReconstructionDistanceAccuracy) + fReconstructionDistanceAccuracy) + std::floor((distance / fReconstructionDistanceAccuracy) + (fReconstructionDistanceAccuracy / 2.)); //clever way of rounding to nearest multipicity of accuracy digit and change it to int
-        if (distanceRound >= maxDistanceNumber)
-          distanceRound = maxDistanceNumber - 1;
-        int thetaNumber = std::round(theta / fReconstructionAngleStep);                                                                                                                                                                // round because of floating point
-        currentValueInSinogram = ++fSinogram->at(distanceRound).at(thetaNumber);                                                                                                                                                       // add to sinogram
-        if (currentValueInSinogram >= fMaxValueInSinogram)
-          fMaxValueInSinogram = currentValueInSinogram;                                    // save max value of sinogram
-      }
-    }
-  }
-  std::ofstream res(fOutFileName);
-  res << "P2" << std::endl;
-  res << (*fSinogram)[0].size() << " " << fSinogram->size() << std::endl;
-  res << fMaxValueInSinogram << std::endl;
-  for (unsigned int i = 0; i < fSinogram->size(); i++) {
-    for (unsigned int j = 0; j < (*fSinogram)[0].size(); j++) {
-      res << (*fSinogram)[i][j] << " ";
-    }
-    res << std::endl;
-  }
-  res.close();
-  BOOST_REQUIRE(JPetCommonTools::ifFileExisting(fOutFileName));
-}*/
 
 BOOST_AUTO_TEST_SUITE_END()
