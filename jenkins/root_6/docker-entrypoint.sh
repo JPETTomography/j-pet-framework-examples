c18370ad9a3be659cb6653c8a360a369e2892f8f
Kamil Rakoczy <grey277@wp.pl>
2018-04-23 11:04:07 +0200

    Add function to executeCommand
    It makes exit script with correct exit code if something fails

--- jenkins/root_6/docker-entrypoint.sh
+++ jenkins/root_6/docker-entrypoint.sh
@@ -1,48 +1,31 @@
-mkdir -p build
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-cd build
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-export CMAKE_LIBRARY_PATH=$CMAKE_LIBRARY_PATH:/framework-dependencies/lib
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-export CMAKE_INCLUDE_PATH=$CMAKE_INCLUDE_PATH:/framework-dependencies/include
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-source /root-system/bin/thisroot.sh
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-cmake ..
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-cd LargeBarrelAnalysis
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-make
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-wget http://sphinx.if.uj.edu.pl/~alek/framework_integration_tests/dabc_17025151847.hld
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-wget http://sphinx.if.uj.edu.pl/~alek/framework_integration_tests/setupRun3.json
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-./LargeBarrelAnalysis.x -t hld -f dabc_17025151847.hld -l setupRun3.json -i 3 -r 0 100
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-mkdir outdir
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-./LargeBarrelAnalysis.x -t hld -f dabc_17025151847.hld -l setupRun3.json -i 3 -r 0 100 -o outdir
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-./LargeBarrelAnalysis.x -t root -f dabc_17025151847.hld.root -l setupRun3.json -i 3 -r 0 100
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-sed -i 's/manager.useTask("TimeWindowCreator", "hld", "tslot.raw");//' ../../LargeBarrelAnalysis/main.cpp
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-sed -i 's/manager.useTask("TimeCalibLoader", "tslot.raw", "tslot.calib");//' ../../LargeBarrelAnalysis/main.cpp
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-make
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-./LargeBarrelAnalysis.x -t root -f dabc_17025151847.tslot.calib.root   -r 0 100
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-cd ../TimeCalibration
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-make
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-cd ../VelocityCalibration
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-make
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-cd ../NewAnalysisTemplate
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-make
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+#!/bin/bash
+
+function executeCommand {
+    $@
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+}
+
+executeCommand "mkdir -p build"
+executeCommand "cd build"
+executeCommand "export CMAKE_LIBRARY_PATH=$CMAKE_LIBRARY_PATH:/framework-dependencies/lib"
+executeCommand "export CMAKE_INCLUDE_PATH=$CMAKE_INCLUDE_PATH:/framework-dependencies/include"
+executeCommand "source /root-system/bin/thisroot.sh"
+executeCommand "cmake .."
+executeCommand "cd LargeBarrelAnalysis"
+executeCommand "make"
+executeCommand "wget http://sphinx.if.uj.edu.pl/~alek/framework_integration_tests/dabc_17025151847.hld"
+executeCommand "wget http://sphinx.if.uj.edu.pl/~alek/framework_integration_tests/setupRun3.json"
+executeCommand "./LargeBarrelAnalysis.x -t hld -f dabc_17025151847.hld -l setupRun3.json -i 3 -r 0 100"
+executeCommand "mkdir outdir"
+executeCommand "./LargeBarrelAnalysis.x -t hld -f dabc_17025151847.hld -l setupRun3.json -i 3 -r 0 100 -o outdir"
+executeCommand "./LargeBarrelAnalysis.x -t root -f dabc_17025151847.hld.root -l setupRun3.json -i 3 -r 0 100"
+executeCommand "sed -i 's/manager.useTask(\"TimeWindowCreator\", \"hld\", \"tslot.raw\");//' ../../LargeBarrelAnalysis/main.cpp"
+executeCommand "sed -i 's/manager.useTask(\"TimeCalibLoader\", \"tslot.raw\", \"tslot.calib\");//' ../../LargeBarrelAnalysis/main.cpp"
+executeCommand "make"
+executeCommand "./LargeBarrelAnalysis.x -t root -f dabc_17025151847.tslot.calib.root   -r 0 100"
+executeCommand "cd ../TimeCalibration"
+executeCommand "make"
+executeCommand "cd ../VelocityCalibration"
+executeCommand "make"
+executeCommand "cd ../NewAnalysisTemplate"
+executeCommand "make"
