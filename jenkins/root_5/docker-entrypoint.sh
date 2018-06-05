d8fb23438e85b02ee2fb9e3e9f29ed2a4f77e51e
Kamil Rakoczy <grey277@wp.pl>
2018-06-05 22:51:41 +0200

    Fix docker-entrypoint

--- jenkins/root_5/docker-entrypoint.sh
+++ jenkins/root_5/docker-entrypoint.sh
@@ -12,7 +12,7 @@ executeCommand "export CMAKE_LIBRARY_PATH=$CMAKE_LIBRARY_PATH:/framework-depende
 executeCommand "export CMAKE_INCLUDE_PATH=$CMAKE_INCLUDE_PATH:/framework-dependencies/include"
 executeCommand "source /root-system/bin/thisroot.sh"
 executeCommand "cmake .."
-executeCommand "make .."
+executeCommand "make"
 executeCommand "cd LargeBarrelAnalysis"
 executeCommand "wget http://sphinx.if.uj.edu.pl/~alek/framework_integration_tests/dabc_17025151847.hld"
 executeCommand "wget http://sphinx.if.uj.edu.pl/~alek/framework_integration_tests/setupRun3.json"
