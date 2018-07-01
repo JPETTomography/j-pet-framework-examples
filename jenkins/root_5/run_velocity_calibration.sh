52c58279609e62dd399d6a69f21a86b84df79164
Kamil Rakoczy <grey277@wp.pl>
2018-07-01 12:33:01 +0200

    Remove old results before start

--- jenkins/root_5/run_velocity_calibration.sh
+++ jenkins/root_5/run_velocity_calibration.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-../../build/VelocityCalibration/VelocityCalibration.x -t hld -f ../dataForVelocity/dabc_16315170022.hld -p conf_trb3.xml -u userParams_root5.json -i 2 -o results_root_5&
-../../build/VelocityCalibration/VelocityCalibration.x -t hld -f ../dataForVelocity/dabc_16315215024.hld -p conf_trb3.xml -u userParams_root5.json -i 2 -o results_root_5&
-../../build/VelocityCalibration/VelocityCalibration.x -t hld -f ../dataForVelocity/dabc_16316203534.hld -p conf_trb3.xml -u userParams_root5.json -i 2 -o results_root_5&
-../../build/VelocityCalibration/VelocityCalibration.x -t hld -f ../dataForVelocity/dabc_16317101413.hld -p conf_trb3.xml -u userParams_root5.json -i 2 -o results_root_5&
-../../build/VelocityCalibration/VelocityCalibration.x -t hld -f ../dataForVelocity/dabc_16317230553.hld -p conf_trb3.xml -u userParams_root5.json -i 2 -o results_root_5&
+../../build/VelocityCalibration/VelocityCalibration.x -t root -f ../dataForVelocity/dabc_16315170022.hld.root -p conf_trb3.xml -u userParams_root5.json -i 2 -o results_root_5&
+../../build/VelocityCalibration/VelocityCalibration.x -t root -f ../dataForVelocity/dabc_16315215024.hld.root -p conf_trb3.xml -u userParams_root5.json -i 2 -o results_root_5&
+../../build/VelocityCalibration/VelocityCalibration.x -t root -f ../dataForVelocity/dabc_16316203534.hld.root -p conf_trb3.xml -u userParams_root5.json -i 2 -o results_root_5&
+../../build/VelocityCalibration/VelocityCalibration.x -t root -f ../dataForVelocity/dabc_16317101413.hld.root -p conf_trb3.xml -u userParams_root5.json -i 2 -o results_root_5&
+../../build/VelocityCalibration/VelocityCalibration.x -t root -f ../dataForVelocity/dabc_16317230553.hld.root -p conf_trb3.xml -u userParams_root5.json -i 2 -o results_root_5&
 
 wait
 ../../build/VelocityCalibration/estimateVelocity results_root5.txt calibrationRoot5
