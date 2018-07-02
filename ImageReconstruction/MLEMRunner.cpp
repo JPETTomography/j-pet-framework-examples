402197917242e59aecdd8f19ba3b661e055b3c81
Kamil Rakoczy <grey277@wp.pl>
2018-07-02 15:26:14 +0200

    Add new line

--- ImageReconstruction/MLEMRunner.cpp
+++ ImageReconstruction/MLEMRunner.cpp
@@ -23,7 +23,7 @@ using namespace jpet_options_tools;
 
 using Point = PET2D::Point<float>;
 
-MLEMRunner::MLEMRunner(const char* name) : JPetUserTask(name) {}
+MLEMRunner::MLEMRunner(const char *name) : JPetUserTask(name) {}
 
 MLEMRunner::~MLEMRunner() {}
 
@@ -36,10 +36,12 @@ bool MLEMRunner::init()
 
 bool MLEMRunner::exec()
 {
-  if (const auto& timeWindow = dynamic_cast<const JPetTimeWindow* const>(fEvent)) {
+  if (const auto &timeWindow = dynamic_cast<const JPetTimeWindow *const>(fEvent))
+  {
     const unsigned int numberOfEventsInTimeWindow = timeWindow->getNumberOfEvents();
-    for (unsigned int i = 0; i < numberOfEventsInTimeWindow; i++) {
-      const auto event = dynamic_cast<const JPetEvent&>(timeWindow->operator[](static_cast<int>(i)));
+    for (unsigned int i = 0; i < numberOfEventsInTimeWindow; i++)
+    {
+      const auto event = dynamic_cast<const JPetEvent &>(timeWindow->operator[](static_cast<int>(i)));
       parseEvent(event);
     }
   }
@@ -52,10 +54,11 @@ bool MLEMRunner::terminate()
   return true;
 }
 
-bool MLEMRunner::parseEvent(const JPetEvent& event)
+bool MLEMRunner::parseEvent(const JPetEvent &event)
 {
   const auto hits = event.getHits();
-  if (hits.size() != 2) {
+  if (hits.size() != 2)
+  {
     return false;
   }
   float x1 = hits[0].getPosX();
@@ -71,30 +74,34 @@ bool MLEMRunner::parseEvent(const JPetEvent& event)
   fOutputStreamTest << std::setprecision(30) << x1 << "\t" << y1 << "\t" << z1 << "\t" << t1 << "\t"
                     << x2 << "\t" << y2 << "\t" << z2 << "\t" << t2 << std::endl;
 
-  if (std::abs(z1 * cm) > fHalfStripLenght || std::abs(z2 * cm) > fHalfStripLenght) {
+  if (std::abs(z1 * cm) > fHalfStripLenght || std::abs(z2 * cm) > fHalfStripLenght)
+  {
     return false;
   }
 
   int d1 = -1, d2 = -1;
-  for (size_t i = 0; i < fScanner.size(); ++i) {
+  for (size_t i = 0; i < fScanner.size(); ++i)
+  {
     if (fScanner[i].contains(Point(x1 * cm, y1 * cm), EPSILON))
       d1 = i;
     if (fScanner[i].contains(Point(x2 * cm, y2 * cm), EPSILON))
       d2 = i;
   }
 
-  if (d1 < 0 || d2 < 0) {
+  if (d1 < 0 || d2 < 0)
+  {
     return false;
   }
 
-  if (d1 < d2) {
+  if (d1 < d2)
+  {
     std::swap(d1, d2);
     std::swap(z1, z2);
     std::swap(t1, t2);
   }
   double dl = (t1 - t2) * speed_of_light_m_per_ps;
-  fOutputStream << d1 << " " << d2 << " " << z1* cm << " "
-                << z2* cm << " " << dl
+  fOutputStream << d1 << " " << d2 << " " << z1 * cm << " "
+                << z2 * cm << " " << dl
                 << "\n";
   return true;
 }
@@ -102,7 +109,8 @@ bool MLEMRunner::parseEvent(const JPetEvent& event)
 void MLEMRunner::setUpOptions()
 {
   auto opts = getOptions();
-  if (isOptionSet(opts, kOutFileNameKey)) {
+  if (isOptionSet(opts, kOutFileNameKey))
+  {
     fOutFileName = getOptionAsString(opts, kOutFileNameKey);
   }
 
@@ -115,9 +123,10 @@ void MLEMRunner::setUpOptions()
 
   const std::vector<float> rotation{0.f, 0.5f, 0.5f}; // rotation for BigBarrel
 
-  const JPetParamBank& bank = getParamBank();
+  const JPetParamBank &bank = getParamBank();
   const JPetGeomMapping mapping(bank);
-  for (unsigned int i = 1; i < mapping.getLayersCount(); i++) {
+  for (unsigned int i = 1; i < mapping.getLayersCount(); i++)
+  {
     radius.push_back(mapping.getRadiusOfLayer(i) * cm);
     scintillators.push_back(static_cast<int>(mapping.getSlotsCount(i)));
   }
@@ -131,12 +140,12 @@ void MLEMRunner::setUpOptions()
   const float fowRadius = 0.4f;
 
   fScanner = ScannerBuilder<SquareScanner>::build_multiple_rings(__PET2D_BARREL(
-               radius,         //radius
-               rotation,       //rotation
-               scintillators,  //n-detectors
-               detectorWidth,  //w-detector
-               detectorHeight, //h-detector
-               detectorD,      //should be d-detector
-               fowRadius       //fow radius
-             ));
+      radius,         //radius
+      rotation,       //rotation
+      scintillators,  //n-detectors
+      detectorWidth,  //w-detector
+      detectorHeight, //h-detector
+      detectorD,      //should be d-detector
+      fowRadius       //fow radius
+      ));
 }
