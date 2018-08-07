f2060e5d2cbcfc76a313a3e5cb1a7d1b821fb51b
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2018-08-07 13:24:23 +0200

    Small corrections to the code to get rid of the compilation warnings
    The changes should not affect the logic of the existing code.

--- LargeBarrelAnalysis/HitFinderTools.cpp
+++ LargeBarrelAnalysis/HitFinderTools.cpp
@@ -26,28 +26,29 @@ using namespace std;
  * Method mapping Signals according to Scintillator they belong to
  */
 map<int, vector<JPetPhysSignal>> HitFinderTools::getSignalsSlotMap(
-  const JPetTimeWindow* timeWindow){
+                                const JPetTimeWindow* timeWindow)
+{
 
-    map<int, vector<JPetPhysSignal>> signalSlotMap;
-    if (!timeWindow) {
-      WARNING("Pointer of Time Window object is not set, returning empty map");
-      return signalSlotMap;
-    }
-
-    const unsigned int nSignals = timeWindow->getNumberOfEvents();
-    for (unsigned int i = 0; i < nSignals; i++) {
-      JPetPhysSignal physSig = dynamic_cast<const JPetPhysSignal&>(timeWindow->operator[](i));
-      int scinID = physSig.getPM().getScin().getID();
-      auto search = signalSlotMap.find(scinID);
-      if (search == signalSlotMap.end()) {
-        vector<JPetPhysSignal> tmp;
-        tmp.push_back(physSig);
-        signalSlotMap.insert(pair<int, vector<JPetPhysSignal>>(scinID, tmp));
-      } else {
-        search->second.push_back(physSig);
-      }
-    }
+  map<int, vector<JPetPhysSignal>> signalSlotMap;
+  if (!timeWindow) {
+    WARNING("Pointer of Time Window object is not set, returning empty map");
     return signalSlotMap;
+  }
+
+  const unsigned int nSignals = timeWindow->getNumberOfEvents();
+  for (unsigned int i = 0; i < nSignals; i++) {
+    JPetPhysSignal physSig = dynamic_cast<const JPetPhysSignal&>(timeWindow->operator[](i));
+    int scinID = physSig.getPM().getScin().getID();
+    auto search = signalSlotMap.find(scinID);
+    if (search == signalSlotMap.end()) {
+      vector<JPetPhysSignal> tmp;
+      tmp.push_back(physSig);
+      signalSlotMap.insert(pair<int, vector<JPetPhysSignal>>(scinID, tmp));
+    } else {
+      search->second.push_back(physSig);
+    }
+  }
+  return signalSlotMap;
 }
 
 /**
@@ -63,9 +64,9 @@ vector<JPetHit> HitFinderTools::matchSignals(
 {
   vector<JPetHit> allHits;
   vector<JPetPhysSignal> remainSignals;
-  for(auto& physSigPair : signalSlotMap) {
-    if(physSigPair.first == refDetScinId) {
-      for(auto refSignal : physSigPair.second){
+  for (auto& physSigPair : signalSlotMap) {
+    if (physSigPair.first == refDetScinId) {
+      for (auto refSignal : physSigPair.second) {
         JPetHit refHit = createDummyRefDetHit(refSignal);
         allHits.push_back(refHit);
       }
@@ -73,37 +74,37 @@ vector<JPetHit> HitFinderTools::matchSignals(
     }
     vector<JPetPhysSignal> sigsVec = physSigPair.second;
     sortByTime(sigsVec);
-    while(sigsVec.size()>0){
-      if(sigsVec.size()==1){
-        if(saveHistos) stats.getHisto1D("remain_signals_per_scin")
+    while (sigsVec.size() > 0) {
+      if (sigsVec.size() == 1) {
+        if (saveHistos) stats.getHisto1D("remain_signals_per_scin")
           ->Fill((float)(sigsVec.at(0).getPM().getScin().getID()));
-        sigsVec.erase(sigsVec.begin()+0);
+        sigsVec.erase(sigsVec.begin() + 0);
         break;
       }
-      for(uint j=1;j<sigsVec.size();j++){
-        if(sigsVec.at(j).getTime()-sigsVec.at(0).getTime()<timeDiffAB){
-          if(sigsVec.at(0).getPM().getSide()!=sigsVec.at(j).getPM().getSide()){
-            JPetHit hit = createHit(sigsVec.at(0),sigsVec.at(j),velocitiesMap);
+      for (uint j = 1; j < sigsVec.size(); j++) {
+        if (sigsVec.at(j).getTime() - sigsVec.at(0).getTime() < timeDiffAB) {
+          if (sigsVec.at(0).getPM().getSide() != sigsVec.at(j).getPM().getSide()) {
+            JPetHit hit = createHit(sigsVec.at(0), sigsVec.at(j), velocitiesMap);
             allHits.push_back(hit);
-            if(saveHistos){
+            if (saveHistos) {
               stats.getHisto2D("time_diff_per_scin")->Fill(
                 hit.getTimeDiff(), (float)(hit.getScintillator().getID()));
               stats.getHisto2D("hit_pos_per_scin")->Fill(
                 hit.getPosZ(), (float)(hit.getScintillator().getID()));
             }
-            sigsVec.erase(sigsVec.begin()+j);
-            sigsVec.erase(sigsVec.begin()+0);
+            sigsVec.erase(sigsVec.begin() + j);
+            sigsVec.erase(sigsVec.begin() + 0);
             break;
-          }else{
-            if(j==sigsVec.size()-1){
-              sigsVec.erase(sigsVec.begin()+0);
+          } else {
+            if (j == sigsVec.size() - 1) {
+              sigsVec.erase(sigsVec.begin() + 0);
               break;
             } else continue;
           }
-        }else{
-          if(saveHistos) stats.getHisto1D("remain_signals_per_scin")
+        } else {
+          if (saveHistos) stats.getHisto1D("remain_signals_per_scin")
             ->Fill((float)(sigsVec.at(0).getPM().getScin().getID()));
-          sigsVec.erase(sigsVec.begin()+0);
+          sigsVec.erase(sigsVec.begin() + 0);
           break;
         }
       }
@@ -122,7 +123,7 @@ JPetHit HitFinderTools::createHit(
 {
   JPetPhysSignal signalA;
   JPetPhysSignal signalB;
-  if(signal1.getPM().getSide()==JPetPM::SideA){
+  if (signal1.getPM().getSide() == JPetPM::SideA) {
     signalA = signal1;
     signalB = signal2;
   } else {
@@ -132,7 +133,7 @@ JPetHit HitFinderTools::createHit(
   auto radius = signalA.getPM().getBarrelSlot().getLayer().getRadius();
   auto theta = TMath::DegToRad() * signalA.getPM().getBarrelSlot().getTheta();
   auto velocity = UniversalFileLoader::getConfigurationParameter(
-    velocitiesMap, getProperChannel(signalA));
+                    velocitiesMap, getProperChannel(signalA));
   checkTheta(theta);
 
   JPetHit hit;
@@ -176,16 +177,18 @@ JPetHit HitFinderTools::createDummyRefDetHit(const JPetPhysSignal& signalB)
  */
 void HitFinderTools::checkTheta(const double& theta)
 {
-  if (theta > 2*TMath::Pi())
+  if (theta > 2 * TMath::Pi()) {
     WARNING("Probably wrong values of Barrel Slot theta - conversion to radians failed. Check please.");
+  }
 }
 
 /**
  * Helper method for getting TOMB channel for velocity map
  */
-int HitFinderTools::getProperChannel(const JPetPhysSignal& signal){
+int HitFinderTools::getProperChannel(const JPetPhysSignal& signal)
+{
   JPetSigCh someSigCh = signal.getRecoSignal().getRawSignal()
-    .getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrNum)[0];
+                        .getPoints(JPetSigCh::Leading, JPetRawSignal::ByThrNum)[0];
   return someSigCh.getTOMBChannel().getChannel();
 }
 
@@ -195,8 +198,8 @@ int HitFinderTools::getProperChannel(const JPetPhysSignal& signal){
 void HitFinderTools::sortByTime(vector<JPetPhysSignal>& sigVec)
 {
   sort(sigVec.begin(), sigVec.end(),
-    [](const JPetPhysSignal & sig1, const JPetPhysSignal & sig2) {
-      return sig1.getTime() < sig2.getTime();
-    }
-  );
+  [](const JPetPhysSignal & sig1, const JPetPhysSignal & sig2) {
+    return sig1.getTime() < sig2.getTime();
+  }
+      );
 }
