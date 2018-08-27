6152a4994a9ecb64d72354537ddd7a1269eb223b
Aleksander Gajos <alek.gajos@gmail.com>
2018-08-27 16:03:52 +0200

    Minor style corrections, only whitespace

--- InterThresholdCalibration/InterThresholdCalibration.cpp
+++ InterThresholdCalibration/InterThresholdCalibration.cpp
@@ -9,7 +9,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ****
+ *
  *  @file InterThresholdCalibration.cpp
  */
 #include <iostream>
@@ -42,41 +42,34 @@ InterThresholdCalibration::~InterThresholdCalibration()
 
 bool InterThresholdCalibration::init()
 {
-  
-
   JPetTimer timer;
 
   string local_time = timer.getAllMeasuredTimes();
 
-////////////////////////
-
   fBarrelMap = new JPetGeomMapping(getParamBank());
 
-  for( int layer = 0; layer < getParamBank().getLayersSize(); layer++)
-  {
+  for ( int layer = 0; layer < getParamBank().getLayersSize(); layer++) {
     kSl_max.push_back(fBarrelMap->getSlotsCount( getParamBank().getLayer(layer) ) );
   }
 
   fOutputEvents = new JPetTimeWindow("JPetEvent");
 
-//This line has to be added since starting from v6 we use cointainers
+  //This line has to be added since starting from v6 we use cointainers
   //for TimeWindows and we need to specify which type of data will be stored
   //in the root tree. For now we do not need to save tree after the calibration
   //so I left the JPetEvent class. For more info ask Alek or Wojtek
 
-//------Calibration run number from options file
-  if ( isOptionSet(fParams.getOptions(), fCalibRunKey)) 
+  //------Calibration run number from options file
+  if ( isOptionSet(fParams.getOptions(), fCalibRunKey))
     fCalibRun = getOptionAsInt(fParams.getOptions(), fCalibRunKey);
-  
 
-  if ( isOptionSet(fParams.getOptions(), fOutputFileKey)) 
+  if ( isOptionSet(fParams.getOptions(), fOutputFileKey))
     fOutputFile = getOptionAsString(fParams.getOptions(), fOutputFileKey);
-  
 
-  if ( isOptionSet(fParams.getOptions(), fFrac_errKey)) 
+  if ( isOptionSet(fParams.getOptions(), fFrac_errKey))
     fFrac_err = getOptionAsDouble(fParams.getOptions(), fFrac_errKey);
 
-  if ( isOptionSet(fParams.getOptions(), fMin_evKey)) 
+  if ( isOptionSet(fParams.getOptions(), fMin_evKey))
     fMin_ev = getOptionAsDouble(fParams.getOptions(), fMin_evKey);
 
   std::ofstream output;
@@ -95,18 +88,14 @@ bool InterThresholdCalibration::init()
 
   //histograms
 
-  for (int lay = 1; lay <= kSl_max.size()+1; lay++) { // loop over layers
-
+  for (int lay = 1; lay <= kSl_max.size() + 1; lay++) { // loop over layers
     for (int sl = 1; sl <= kSl_max[lay - 1]; sl++) { // loop over slots
-
       for (int thre = 2; thre <= 4; thre++) { // loop over th diffr times
-
-
+	
 //leading
 //histos for side A
         const char* histo_name_l_A = Form("timeDiffA_leading_layer_%d_slot_%d_thr_1%d", lay, sl, thre);
         getStatistics().createHistogram( new TH1F(histo_name_l_A, histo_name_l_A, 200, -2., 2.));
-
 
 //histos for side B
         const char* histo_name_l_B = Form("timeDiffB_leading_layer_%d_slot_%d_thr_1%d", lay, sl, thre);
@@ -116,16 +105,13 @@ bool InterThresholdCalibration::init()
 //histos for side A
         const char* histo_name_t_A = Form("timeDiffA_trailing_layer_%d_slot_%d_thr_1%d", lay, sl, thre);
         getStatistics().createHistogram( new TH1F(histo_name_t_A, histo_name_t_A, 200, -2., 2.));
-
-
+	
 //histos for side B
         const char* histo_name_t_B = Form("timeDiffB_trailing_layer_%d_slot_%d_thr_1%d", lay, sl, thre);
         getStatistics().createHistogram(new TH1F(histo_name_t_B, histo_name_t_B, 300, -3., 3.));
-
       }
     }
   }
-
 
   INFO("#############");
   INFO("CALIB_INIT: INITIALIZATION DONE!");
@@ -134,15 +120,9 @@ bool InterThresholdCalibration::init()
   return true;
 }
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 bool InterThresholdCalibration::exec()
 {
-
-
   std::vector <JPetHit> fhitsCalib;
-
 
   //getting the data from event in propriate format
   if (auto timeWindow = dynamic_cast<const JPetTimeWindow* const>(fEvent)) {
@@ -155,66 +135,48 @@ bool InterThresholdCalibration::exec()
 
     }
 
-
-
     for (auto i = fhitsCalib.begin(); i != fhitsCalib.end(); i++) {
       fillHistosForHit(*i);
     }
 
     fhitsCalib.clear();
 
-  }
-
-
-
-  else {
+  } else {
     return false;
   }
-
+  
   return true;
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 bool InterThresholdCalibration::terminate()
 {
-
-  //
-  //create output txt file with calibration parameters
-  //
+  // create output txt file with calibration parameters
   std::ofstream results_fit;
   results_fit.open(fOutputFile, std::ios::app);
-  //
-  for (int lay = 1; lay <= kSl_max.size()+1; lay++) { // loop over layers
 
+  for (int lay = 1; lay <= kSl_max.size() + 1; lay++) { // loop over layers
     for (int sl = 1; sl <= kSl_max[lay - 1]; sl++) { // loop over slots
-
       for (int th = 1; th <= 3; th++) { // loop over th diffr times
 
-//leading
-//histos for side A
+        //leading
+        //histos for side A
         const char* histo_name_l_A = Form("timeDiffA_leading_layer_%d_slot_%d_thr_1%d", lay, sl, th + 1);
         TH1F* histoToSave_leading_A = getStatistics().getHisto1D(histo_name_l_A);
-
-
-//histos for side B
+                
+        //histos for side B
         const char* histo_name_l_B = Form("timeDiffB_leading_layer_%d_slot_%d_thr_1%d", lay, sl, th + 1);
         TH1F* histoToSave_leading_B = getStatistics().getHisto1D(histo_name_l_B);
 
-//trailing
-//histos for side A
+        //trailing
+        //histos for side A
         const char* histo_name_t_A = Form("timeDiffA_trailing_layer_%d_slot_%d_thr_1%d", lay, sl, th + 1);
         TH1F* histoToSave_trailing_A = getStatistics().getHisto1D(histo_name_t_A);
-
-
-//histos for side B
+        
+        //histos for side B
         const char* histo_name_t_B = Form("timeDiffB_trailing_layer_%d_slot_%d_thr_1%d", lay, sl, th + 1);
         TH1F* histoToSave_trailing_B = getStatistics().getHisto1D(histo_name_t_B);
-
-
-//
-//minimal criteria for histograms
-
+        
+	//minimal criteria for histograms
         if (histoToSave_leading_A->GetEntries() != 0 && histoToSave_leading_B->GetEntries() != 0
             && histoToSave_trailing_A->GetEntries() != 0 && histoToSave_trailing_B->GetEntries() != 0) {
           INFO("#############");
@@ -241,8 +203,7 @@ bool InterThresholdCalibration::terminate()
             WARNING(": Statistics used to determine the trailing edge (B) threshold calibration constant was less than " + std::to_string(fMin_ev) + " events!");
           }
 
-
-//fit scintilators
+	  //fit scintilators
           double highestBin_l_A = histoToSave_leading_A->GetBinCenter(histoToSave_leading_A->GetMaximumBin());
           histoToSave_leading_A->Fit("gaus", "", "", highestBin_l_A - 0.2, highestBin_l_A + 0.2);
           histoToSave_leading_A->Draw();
@@ -258,7 +219,6 @@ bool InterThresholdCalibration::terminate()
           double highestBin_t_B = histoToSave_trailing_B->GetBinCenter(histoToSave_trailing_B->GetMaximumBin());
           histoToSave_trailing_B->Fit("gaus", "", "", highestBin_t_B - 0.2, highestBin_t_B + 0.2);
           histoToSave_trailing_B->Draw();
-
 
           TF1* fit_l_A = histoToSave_leading_A->GetFunction("gaus");
           TF1* fit_l_B = histoToSave_leading_B->GetFunction("gaus");
@@ -302,29 +262,23 @@ bool InterThresholdCalibration::terminate()
             results_fit << "#WFIT: Large uncertainty on the calibration constant!" << endl;
           }
 
-
-// writing to apropriate format (txt file)
-
-//side A
+	  // writing to apropriate format (txt file)
+	  //side A
           results_fit << lay << "\t" << sl << "\t" << "A" << "\t" << th << "\t" << position_peak_l_A << "\t" << position_peak_error_l_A << "\t" << position_peak_t_A << "\t" << position_peak_error_t_A << "\t" << sigma_peak_l_A
                       << "\t" << sigma_peak_t_A << "\t"  << chi2_ndf_l_A << "\t" << chi2_ndf_t_A << endl;
-
-//side B
+	  
+	  //side B
           results_fit << lay << "\t" << sl << "\t" << "B" << "\t" << th << "\t" << position_peak_l_B << "\t" << position_peak_error_l_B << "\t" << position_peak_t_B << "\t" << position_peak_error_t_B << "\t" << sigma_peak_l_B
                       << "\t" << sigma_peak_t_B << "\t"  << chi2_ndf_l_B << "\t" << chi2_ndf_t_B << endl;
-
+	  
         } else {
           ERROR(": ONE OF THE HISTOGRAMS FOR THRESHOLD " + std::to_string(th) + " LAYER " + std::to_string(lay) + " SLOT " + std::to_string(sl) + " IS EMPTY, WE CANNOT CALIBRATE IT");
         }
-
       }
-
     }
   }
-
-
+  
   results_fit.close();
-
 
   return true;
 }
@@ -340,8 +294,7 @@ void InterThresholdCalibration::fillHistosForHit(const JPetHit& hit)
   auto lead_times_B = hit.getSignalB().getRecoSignal().getRawSignal().getTimesVsThresholdNumber(JPetSigCh::Leading);
   auto trail_times_B = hit.getSignalB().getRecoSignal().getRawSignal().getTimesVsThresholdNumber(JPetSigCh::Trailing);
 
-
-//take slot number for the hit
+  //take slot number for the hit
   int slot_number = hit.getBarrelSlot().getID();
   int layer_number = hit.getBarrelSlot().getLayer().getID();
   int slot_nr;
@@ -355,12 +308,11 @@ void InterThresholdCalibration::fillHistosForHit(const JPetHit& hit)
   double lead_times_first_A, lead_times_first_B;
   double trail_times_first_A, trail_times_first_B;
 
+  // leading edge
 
-//leading edge
-
-//A
-  for (auto & thr_time_pair : lead_times_A) {
-
+  // A
+  for (auto& thr_time_pair : lead_times_A) {
+    
     int thr = thr_time_pair.first;
 
     if (lead_times_A.count(thr) > 0 && trail_times_A.count(thr) > 0 && lead_times_A.size() == 4 && trail_times_A.size() == 4) { //exactly 4 thresholds
@@ -368,19 +320,17 @@ void InterThresholdCalibration::fillHistosForHit(const JPetHit& hit)
       lead_times_first_A = lead_times_A[1];
 
       if (thr >= 2) {
-
+	
         thr_time_diff_A[thr] = lead_times_A[thr] / 1000 - lead_times_first_A / 1000;
-
+	
         char* histo_name_l_A = Form("timeDiffA_leading_layer_%d_slot_%d_thr_1%d", layer_number, slot_nr, thr);
         getStatistics().getHisto1D(histo_name_l_A)->Fill(thr_time_diff_A[thr]);
-
       }
-
     }
   }
-
-//B
-  for (auto & thr_time_pair : lead_times_B) {
+  
+  // B
+  for (auto& thr_time_pair : lead_times_B) {
 
     int thr = thr_time_pair.first;
 
@@ -394,22 +344,19 @@ void InterThresholdCalibration::fillHistosForHit(const JPetHit& hit)
 
         char* histo_name_l_B = Form("timeDiffB_leading_layer_%d_slot_%d_thr_1%d", layer_number, slot_nr, thr);
         getStatistics().getHisto1D(histo_name_l_B)->Fill(thr_time_diff_B[thr]);
-
       }
-
     }
   }
 
 
-//trailing edge
-  for (auto & thr_time_pair : trail_times_A) {
+  // trailing edge
+  for (auto& thr_time_pair : trail_times_A) {
 
     int thr = thr_time_pair.first;
 
     if (trail_times_A.count(thr) > 0 && lead_times_A.count(thr) > 0 && lead_times_A.size() == 4 && trail_times_A.size() == 4) {
 
       trail_times_first_A = trail_times_A[1];
-
 
       if (thr >= 2) {
 
@@ -419,12 +366,11 @@ void InterThresholdCalibration::fillHistosForHit(const JPetHit& hit)
         getStatistics().getHisto1D(histo_name_t_A)->Fill(thr_time_diff_t_A[thr]);
 
       }
-
     }
   }
 
 
-  for (auto & thr_time_pair : trail_times_B) {
+  for (auto& thr_time_pair : trail_times_B) {
 
     int thr = thr_time_pair.first;
 
@@ -441,20 +387,16 @@ void InterThresholdCalibration::fillHistosForHit(const JPetHit& hit)
         getStatistics().getHisto1D(histo_name_t_B)->Fill(thr_time_diff_t_B[thr]);
 
       }
-
     }
   }
-
-
+  
 }
 
 
 const char* InterThresholdCalibration::formatUniqueSlotDescription(const JPetBarrelSlot& slot, int threshold, const char* prefix = "")
 {
-
   int slot_number = fBarrelMap->getSlotNumber(slot);
   int layer_number = fBarrelMap->getLayerNumber(slot.getLayer());
 
   return Form("%slayer_%d_slot_%d_thr_%d", prefix, layer_number, slot_number, threshold);
-
 }
