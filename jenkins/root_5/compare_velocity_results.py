221493c180552cca3efafd08094ee824fa228d0b
Kamil Rakoczy <grey277@wp.pl>
2018-12-21 11:55:41 +0100

    Fix comparing results script

--- jenkins/root_5/compare_velocity_results.py
+++ jenkins/root_5/compare_velocity_results.py
@@ -3,10 +3,20 @@ from __future__ import print_function
 import csv
 import sys
 
-def compare_lines(first_line, second_line):
+
+def compare_lines(first_line, second_line, multiplier):
     value_diff = abs(
-            (float)(first_line[4]) - (float)(second_line[4]))
-    return first_line[0] == second_line[0] and first_line[1] == second_line[1] and first_line[2] == second_line[2] and first_line[3] == second_line[3] and first_line[6] == second_line[6] and first_line[7] == second_line[7] and first_line[8] == second_line[8] and first_line[9] == second_line[9] and first_line[10] == second_line[10] and first_line[11] == second_line[11] and value_diff * (float)(multiplier)) <= abs((float)(input_splitted[5])) and (value_diff * (float)(multiplier)) <= abs((float)(test_splitted[5]) 
+        (float)(first_line[4]) - (float)(second_line[4]))
+    if((int)(first_line[0]) == (int)(second_line[0])):
+        if((int)(first_line[1]) == (int)(second_line[1])):
+            if((str)(first_line[2]) == (str)(second_line[2])):
+                if((int)(first_line[3]) == (int)(second_line[3])):
+                    if((value_diff * (float)(multiplier)) <= abs((float)(first_line[5])) and ((value_diff * (float)(multiplier)) <= abs((float)(second_line[5])))):
+                        return True
+                    else:
+                        print("Value diff %f, inputvalue diff: %s, testvalue diff: %s" %
+                              (value_diff * (float)(multiplier), first_line[4], second_line[4]))
+    return False
 
 
 def parse_file(input_file, test_file, multiplier):
@@ -15,11 +25,9 @@ def parse_file(input_file, test_file, multiplier):
     for (input_line, test_line) in zip(input, test):
         input_splitted = input_line.rstrip().split("\t")
         test_splitted = test_line.rstrip().split("\t")
-        if (compare_lines(input_line, test_line)):
+        if (compare_lines(input_splitted, test_splitted, multiplier)):
             continue
         else:
-            print("Value diff %f, inputvalue diff: %s, testvalue diff: %s" %
-                  (value_diff * (float)(multiplier), input_splitted[2], test_splitted[2]))
             return 1
 
     return 0
