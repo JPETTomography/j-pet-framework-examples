48fb674cf5da92a2116d75abc3ed289715a79c32
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2018-12-29 18:51:33 +0100

    Add the EventCategorizerTools with the new TOF version

--- LargeBarrelAnalysis/EventCategorizerTools.h
+++ LargeBarrelAnalysis/EventCategorizerTools.h
@@ -27,40 +27,36 @@ static const double kUndefinedValue = 999.0;
  * @brief Tools for Event Categorization
  *
  * Lots of tools in constatnt developement.
- */
+*/
 class EventCategorizerTools
 {
 public:
-  static bool checkFor2Gamma(
-    const JPetEvent& event, JPetStatistics& stats, bool saveHistos, double b2bSlotThetaDiff
-  );
-  static bool checkFor3Gamma(
-    const JPetEvent& event, JPetStatistics& stats, bool saveHistos
-  );
-  static bool checkForPrompt(
-    const JPetEvent& event, JPetStatistics& stats, bool saveHistos,
-    double deexTOTCutMin, double deexTOTCutMax
-  );
-  static bool checkForScatter(
-    const JPetEvent& event, JPetStatistics& stats, bool saveHistos, double scatterTOFTimeDiff
-  );
+  static bool checkFor2Gamma(const JPetEvent& event, JPetStatistics& stats,
+                             bool saveHistos, double b2bSlotThetaDiff);
+  static bool checkFor3Gamma(const JPetEvent& event, JPetStatistics& stats, bool saveHistos);
+  static bool checkForPrompt(const JPetEvent& event, JPetStatistics& stats,
+                             bool saveHistos, double deexTOTCutMin, double deexTOTCutMax);
+  static bool checkForScatter(const JPetEvent& event, JPetStatistics& stats,
+                              bool saveHistos, double scatterTOFTimeDiff);
   static double calculateTOT(const JPetHit& hit);
   static double calculateDistance(const JPetHit& hit1, const JPetHit& hit2);
   static double calculateScatteringTime(const JPetHit& hit1, const JPetHit& hit2);
   static double calculateScatteringAngle(const JPetHit& hit1, const JPetHit& hit2);
-  static double calculateTOF(const JPetHit& firstHit, const JPetHit& latterHit);
-  static TVector3 calculateAnnihilationPoint(const JPetHit& firstHit, const JPetHit& latterHit);
-  static double calculatePlaneCenterDistance(
-    const JPetHit& firstHit, const JPetHit& secondHit, const JPetHit& thirdHit
-  );
-  static bool stream2Gamma(
-    const JPetEvent& event, JPetStatistics& stats, bool saveHistos,
-    double b2bSlotThetaDiff, double b2bTimeDiff
-  );
-  static bool stream3Gamma(
-    const JPetEvent& event, JPetStatistics& stats, bool saveHistos,
-    double d3SlotThetaMin, double d3TimeDiff, double d3DistanceFromCenter
-  );
+  /// Tof is calculated as  time1 -time2.
+  static double calculateTOF(const JPetHit& hitA, const JPetHit& hitB);
+  static double calculateTOF(double time1, double time2);
+  /// Tof calculated with the ordered hits with respect to scintillator number.
+  /// The first one will be hit with smaller theta angle.
+  /// See also: http://koza.if.uj.edu.pl/petwiki/index.php/Coordinate_system_in_Big_Barrel
+  static double calculateTOFByConvention(const JPetHit& hitA, const JPetHit& hitB);
+  static TVector3 calculateAnnihilationPoint(const JPetHit& hitA, const JPetHit& hitB);
+  static TVector3 calculateAnnihilationPoint(const TVector3& hitA, const TVector3& hitB, double tof);
+  static double calculatePlaneCenterDistance(const JPetHit& firstHit,
+      const JPetHit& secondHit, const JPetHit& thirdHit);
+  static bool stream2Gamma(const JPetEvent& event, JPetStatistics& stats,
+                           bool saveHistos, double b2bSlotThetaDiff, double b2bTimeDiff);
+  static bool stream3Gamma(const JPetEvent& event, JPetStatistics& stats,
+                           bool saveHistos, double d3SlotThetaMin, double d3TimeDiff, double d3DistanceFromCenter);
 };
 
 #endif /* !EVENTCATEGORIZERTOOLS_H */
