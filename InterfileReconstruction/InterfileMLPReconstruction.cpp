1c384253901764e47efb257b6a0a638c181fd61f
Kamil Rakoczy <kamilrakoczy1@gmail.com>
2021-05-19 23:02:10 +0200

    Refactor saving image to interfile
    Signed-off-by: Kamil Rakoczy <kamilrakoczy1@gmail.com>

--- InterfileReconstruction/InterfileMLPReconstruction.cpp
+++ InterfileReconstruction/InterfileMLPReconstruction.cpp
@@ -1,4 +1,18 @@
+/**
+ *  @copyright Copyright 2021 The J-PET Framework Authors. All rights reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may find a copy of the License in the LICENCE file.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 #include "InterfileMLPReconstruction.h"
+#include "InterfileReconstructionTools.h"
 #include "EventCategorizerTools.h"
 #include <TH3D.h>
 #include <TVector3.h>
@@ -39,8 +53,10 @@ bool InterfileMLPReconstruction::exec()
         auto hits = event.getHits();
         for (unsigned int i = 0; i < hits.size() - 1; i++)
         {
-          TVector3 annihilationPoint = EventCategorizerTools::calculateAnnihilationPoint(hits[i], hits[i + 1]);
-          getStatistics().getObject<TH3D>("hits_pos")->Fill(annihilationPoint.X(), annihilationPoint.Y(), annihilationPoint.Z());
+          for (unsigned int j = i + 1; j < hits.size(); j++) {
+            TVector3 annihilationPoint = EventCategorizerTools::calculateAnnihilationPoint(hits[i], hits[j]);
+            getStatistics().getObject<TH3D>("hits_pos")->Fill(annihilationPoint.X(), annihilationPoint.Y(), annihilationPoint.Z());
+          }
         }
       }
     }
@@ -55,39 +71,7 @@ bool InterfileMLPReconstruction::exec()
 
 bool InterfileMLPReconstruction::terminate()
 {
-  std::ofstream fOutputStream;
-  fOutputStream.open(fOutputPath + ".img", std::ios::binary | std::ios::out);
-
-  auto pos_histo = getStatistics().getObject<TH3D>("hits_pos");
-
-  auto x_bins = pos_histo->GetNbinsX();
-  auto y_bins = pos_histo->GetNbinsY();
-  auto z_bins = pos_histo->GetNbinsZ();
-
-  //Intefile format is defined in a way, that we saves 2D arrays
-  //from top left corner to bottom right
-  //in row major order, more information in article:
-  //https://www.researchgate.net/publication/21707960_A_file_format_for_the_exchange_of_nuclear_medicine_image_data_A_specification_of_interfile_version_33
-
-  for (auto i = 0; i < z_bins; i++) {
-    for (auto j = y_bins; j > 0; j--) {
-      auto bin_pos = pos_histo->GetBin(1, j, i + 1);
-      for (auto k = 0; k < x_bins; k++) {
-        auto val = static_cast<float>(pos_histo->GetBinContent(bin_pos + k));
-        fOutputStream.write(reinterpret_cast<char *>(&val), sizeof(val));
-      }
-    }
-  }
-
-  fOutputStream.close();
-
-  fOutputStream.open(fOutputPath + ".hdr", std::ios::out);
-  fOutputStream << "X size: " << x_bins << " " << std::endl
-                << "Y size: " << y_bins << " " << std::endl
-                << "Z size: " << z_bins << " " << std::endl
-                << "type: float" << std::endl;
-  fOutputStream.close();
-
+  InterfileReconstructionTools::saveToInterfile(getStatistics().getObject<TH3D>("hits_pos"), fOutputPath);
   return true;
 }
 
