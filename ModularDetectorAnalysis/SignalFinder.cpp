3ecd532cdb98e18a23e442bc2d60ee206513ce7b
Krzysztof Kacprzak <k.kacprzak@yahoo.com>
2021-06-02 18:25:23 -0300

    adjusting tot histo limits

--- ModularDetectorAnalysis/SignalFinder.cpp
+++ ModularDetectorAnalysis/SignalFinder.cpp
@@ -194,16 +194,16 @@ void SignalFinder::initialiseHistograms()
   getStatistics().getHisto1D("rawsig_tslot")->GetYaxis()->SetTitle("Number of Time Windows");
 
   getStatistics().createHistogram(new TH2F("tot_sum_sipm_id", "SiPM Signal Time over Threshold per SiPM ID", maxPMID - minPMID + 1, minPMID - 0.5,
-                                           maxPMID + 0.5, 200, 0.0, kNumOfThresholds * fSigChLeadTrailMaxTime));
+                                           maxPMID + 0.5, 200, 0.0, fSigChLeadTrailMaxTime));
   getStatistics().getHisto2D("tot_sum_sipm_id")->GetXaxis()->SetTitle("SiPM ID");
   getStatistics().getHisto2D("tot_sum_sipm_id")->GetYaxis()->SetTitle("TOT [ps]");
 
   getStatistics().createHistogram(new TH2F("tot_sum_sipm_id_norm", "SiPM Signal Time over Threshold per SiPM ID normalised", maxPMID - minPMID + 1,
-                                           minPMID - 0.5, maxPMID + 0.5, 200, 0.0, kNumOfThresholds * fSigChLeadTrailMaxTime));
+                                           minPMID - 0.5, maxPMID + 0.5, 200, 0.0, fSigChLeadTrailMaxTime));
   getStatistics().getHisto2D("tot_sum_sipm_id_norm")->GetXaxis()->SetTitle("SiPM ID");
   getStatistics().getHisto2D("tot_sum_sipm_id_norm")->GetYaxis()->SetTitle("TOT [ps]");
 
-  double totUppLimit = 60.0 * kNumOfThresholds * fSigChLeadTrailMaxTime; // 24000000.0;
+  double totUppLimit = 50. * fSigChLeadTrailMaxTime;
   getStatistics().createHistogram(new TH2F("tot_rec_sipm_id", "SiPM Signal Time over Threshold per SiPM ID", maxPMID - minPMID + 1, minPMID - 0.5,
                                            maxPMID + 0.5, 200, 0.0, totUppLimit));
   getStatistics().getHisto2D("tot_rec_sipm_id")->GetXaxis()->SetTitle("SiPM ID");
--- ModularDetectorAnalysis/SignalTransformer.cpp
+++ ModularDetectorAnalysis/SignalTransformer.cpp
@@ -182,7 +182,7 @@ void SignalTransformer::initialiseHistograms()
   getStatistics().getHisto1D("mtxsig_scin_sideB")->GetXaxis()->SetTitle("Scin ID");
   getStatistics().getHisto1D("mtxsig_scin_sideB")->GetYaxis()->SetTitle("Number of Matrix Signals");
 
-  double totUppLimit = 24000000.0;
+  double totUppLimit = 10000000.0;
   getStatistics().createHistogram(new TH2F("tot_scin_sideA_id", "Matrix Signal ToT - Side A Scin ID", maxScinID - minScinID + 1, minScinID - 0.5,
                                            maxScinID + 0.5, 200, 0.0, totUppLimit));
   getStatistics().getHisto2D("tot_scin_sideA_id")->GetXaxis()->SetTitle("SiPM ID");
