b38fc0607db053db09427353e2ce3cbf684b03ac
Krzysztof Kacprzak <k.kacprzak@yahoo.com>
2022-09-26 13:30:00 -0300

    save sinc hits

--- RedModuleAnalysis/RedModuleHitFinder.cpp
+++ RedModuleAnalysis/RedModuleHitFinder.cpp
@@ -157,8 +157,9 @@ bool RedModuleHitFinder::exec()
     }
 
     // Hits from red module are merged with WLS, so no need to save them twice
+    saveHits(redHits);
     saveHits(refHits);
-    saveHits(wlsHits);
+    // saveHits(wlsHits);
   }
   else
   {
